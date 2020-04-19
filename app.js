b59f36d7ee83dc2a7076f8b281705d495269dd92
aniinad <chow.anil@gmail.com>
2020-04-19 03:27:29 -0400

    Added Authentication

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 var express = require("express");
 var app = express();
+var passport = require("passport");
+var  mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var LocalStrategy = require("passport-local");
+var User = require("./models/user");
 
 var  rPromise = require("request-promise"); 
 app.use(express.static("public"));
+mongoose.connect("mongodb://aniinad:123@cluster0-shard-00-00-rigfj.azure.mongodb.net:27017,cluster0-shard-00-01-rigfj.azure.mongodb.net:27017,cluster0-shard-00-02-rigfj.azure.mongodb.net:27017/mysite?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority");
+app.set("view engine", "ejs");
 
-// app.set("view engine", "ejs");
 
 app.listen(process.env.PORT||5000,function(){
     console.log("Service started");
 });
 
+
+//Passport Configuration
+app.use(require("express-session")({
+    secret:"Nandu is a good boy",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+})
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", function(req,res){
+    console.log(req.user);
     res.render("index.ejs");
 });
 app.get("/home", function(req,res){
@@ -25,6 +52,66 @@ app.get("/contact", function(req,res){
 app.get("/gallary", function(req,res){
     res.render("gallary.ejs");
 });
+
+app.get("/updates",isLoggedIn,function(req,res){
+    res.render("updates.ejs");
+})
+
+//Auth routes
+app.get("/register",function(req,res){
+    res.render("register");
+})
+
+app.post("/register", function(req,res){
+    
+    User.register(new User({username: req.body.username}),
+        req.body.password,function(err, user){
+            if(err)
+            {
+                console.log(err);
+                return res.render("register");
+            }
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/");
+            })
+        })
+})
+
+
+//Login form
+app.get("/login", function(req,res){
+    res.render("login");
+})
+
+app.post("/login",passport.authenticate("local",
+{successRedirect:"/updates",
+failureRedirect:"/login"
+}),function(req,res){
+
+})
+
+//Logout route
+app.get("logout",function(req,res){
+ req.session.destroy((err) => {
+	if(err) return next(err)
+
+	req.logout()
+
+	res.redirect("/");
+})
+    // req.logout();
+    // res.redirect("/");
+})
+
+function isLoggedIn(req,res,next)
+{
+    if(req.isAuthenticated())
+    {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 app.get("/movie", function(req,res){
     //res.render("movieSearch.ejs");
     var searchKey = req.query.search;
